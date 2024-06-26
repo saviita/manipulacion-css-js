8304bc961d38c3d05bcceb36dd6f2ab820d5aa4a
saviita <taatt2000@gmail.com>
2024-06-26 11:32:22 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const formElement = document.getElementById('form');
+
+const checkForm = event => {
+  //para evitar que se recargue despues de enviar
+  event.preventDefault();
+};
+
+formElement.addEventListener('submit', checkForm);
+formElement.addEventListener('input', event => {
+  //te escribe los valores en tiempo real de los inputs
+  console.log(event.target.value);
+});
